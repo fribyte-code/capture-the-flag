6cd571c47a7845088f221936c1cfc539de64aec5
Mathias Haugsbø <mathiash98@gmail.com>
2023-01-18 00:02:24 +0100

    Add basic cookie auth

--- friByte.capture-the-flag.service/friByte.capture-the-flag.service/Controllers/TaskController.cs
+++ friByte.capture-the-flag.service/friByte.capture-the-flag.service/Controllers/TaskController.cs
@@ -1,10 +1,18 @@
 using friByte.capture_the_flag.service.Models;
 using friByte.capture_the_flag.service.Services;
+using friByte.capture_the_flag.service.Services.Auth;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace friByte.capture_the_flag.service.Controllers;
 
+/// <summary>
+/// API endpoints for managing tasks. The management endpoints should be restricted to Admin
+/// List tasks can probably be open for all.
+/// </summary>
+[Authorize(Roles = $"{IdentityRoleNames.TeamRoleName}, {IdentityRoleNames.AdminRoleName}")]
 [ApiController]
+[Produces("application/json")]
 [Route("Api/[controller]")]
 public class 
     TaskController : ControllerBase {
@@ -18,6 +26,10 @@ public class
         _ctfTaskService = ctfTaskService;
     }
 
+    /// <summary>
+    /// Get all tasks in database, currently even the flag is returned
+    /// </summary>
+    /// <returns></returns>
     [HttpGet(Name = "task")]
     public Task<List<CtfTask>> Get()
     {
